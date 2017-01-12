f81343d9b0fa735a25a51a1ab4e3fb03a8c7b5c3
Laure <laure.milian@gmail.com>
2017-01-12 11:42:29 +0100

    affichage du nombre de mots qui font exactement 15 caractères

--- index.php
+++ index.php
@@ -3,11 +3,25 @@
 $string = file_get_contents("dictionnaire.txt", FILE_USE_INCLUDE_PATH);
 $dico = explode("\n", $string);
 
+
 // Combien de mots contient ce dictionnaire ?
 $words_num = count($dico);
-echo "Le dictionnaire compte " . $words_num . " mots."; 
+echo "<div>Le dictionnaire compte " . $words_num . " mots.</div>"; 
+
 
 // Combien de mots font exactement 15 caractères ?
+for ($i = 0; $i < $words_num ; $i++) {
+
+	$word = $dico[$i];
+	$pattern = '(.)su';
+	$length = preg_match_all($pattern, $word, $matches);
+
+	if ($length === 15) {
+		$words_15car[$i] = $matches;	
+	}
+}
+echo "<div>Parmi ceux-ci, " . count($words_15car) . " mots font exactement 15 caractères.</div>";
+
 
 // Combien de mots contiennent la lettre « w » ?
 // Combien de mots finissent par la lettre « q » ?
