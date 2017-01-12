7da1fa8b2c0d8887eaa41debcf93742912816902
Laure <laure.milian@gmail.com>
2017-01-12 11:02:53 +0100

    affichage du nombre de mots contenus dans le dictionnaire

--- index.php
+++ index.php
@@ -3,4 +3,11 @@
 $string = file_get_contents("dictionnaire.txt", FILE_USE_INCLUDE_PATH);
 $dico = explode("\n", $string);
 
-var_dump($dico);
+// Combien de mots contient ce dictionnaire ?
+$words_num = count($dico);
+echo "Le dictionnaire compte " . $words_num . " mots."; 
+
+// Combien de mots font exactement 15 caractères ?
+
+// Combien de mots contiennent la lettre « w » ?
+// Combien de mots finissent par la lettre « q » ?
