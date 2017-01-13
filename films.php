3d6e95d2cfde87d40f03c32fc4254bdc05fa8d55
Laure <laure.milian@gmail.com>
2017-01-13 09:53:42 +0100

    Affiche réalisateurs de the lego movie + ajout explode pour affichage des labels

--- films.php
+++ films.php
@@ -8,22 +8,30 @@ $top = $brut["feed"]["entry"];
 
 <?php
 	//Quel est le classement du film « Gravity » ?
+	//Quel est le réalisateur du film « The LEGO Movie » ?
 	foreach ($top as $key => $movie) {
-		$label = $movie["im:name"]["label"];
+		$label = $movie["title"]["label"];
+		$labels = explode(" - ", $label);
 		$position = $key + 1;
-		if ($label === "Gravity") {
-			echo "Classement de Gravity : " . $position;
+		if ($labels[0] === "Gravity") {
+			echo "<div> Classement de Gravity : " . $position . "</div>";
+		} else if ($labels[0] === "The LEGO Movie") {
+			echo "<div> Résalisateurs de The LEGO Movie : " . $labels[1] . "</div>";
 		};
 	}
 ?>
 
+
+
+
 <ul>
 	<?php
-		//Afficher top100 selon la forme demandée
+	//Afficher top100 selon la forme demandée
 	foreach ($top as $key => $movie) {
-		$label = $movie["im:name"]["label"];
+		$label = $movie["title"]["label"];
+		$labels = explode(" - ", $label);
 		$position = $key + 1;
-		echo "<li>" . $position . " " . $label . "</li>";
+		echo "<li>" . $position . " " . $labels[0] . "</li>";
 	}
 	?>
 </ul>
