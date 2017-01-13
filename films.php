cabc7cb5c91965c0208619b2cdcdec5f75ad6a7c
Laure <laure.milian@gmail.com>
2017-01-13 09:31:15 +0100

    affichage du top 100 de films en liste

--- films.php
+++ films.php
@@ -3,4 +3,16 @@
 $string = file_get_contents("films.json", FILE_USE_INCLUDE_PATH);
 $brut = json_decode($string, true);
 $top = $brut["feed"]["entry"];
-var_dump($top);
+
+?>
+
+<ul>
+	<?php
+		//Afficher top100 selon la forme demandée
+		foreach ($top as $key => $movie) {
+			$label = $movie["im:name"]["label"];
+			$position = $key + 1;
+			echo "<li>" . $position . " " . $label . "</li>";
+		}
+	?>
+</ul>
